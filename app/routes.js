cbea323d1b504271900300e2d3cc2cbf97104df3
Chris Patuzzo <chris@patuzzo.co.uk>
2017-10-24 15:44:20 +0100

    Initial commit - add page elements with dynamic data

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,34 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
-// add your routes here
+var data = {
+  address: 'foo@example.com',
+  preferences: [
+    {
+      name: 'daily',
+      value: 'together',
+    },
+    {
+      name: 'weekly',
+      value: 'separately',
+    },
+  ],
+  subscriptions: [
+    {
+      title: 'Publications from the Department for Education',
+      frequency: 'daily',
+      channel: 'HTML email',
+    },
+    {
+      title: 'Spain Travel Alerts',
+      frequency: 'immediately',
+      channel: 'text message',
+    },
+  ]
+}
+
+router.get('/manage', function (req, res) {
+  res.render('manage', data);
+})
 
 module.exports = router
